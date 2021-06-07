6574aba76724ad6dc38b88157003bcfaa501c6a4
ayokunle adeleye <adeways2000@gmail.com>
2021-06-07 14:06:22 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println(PaintJob.getBucketCount(-3.4,2.1,1.5,2));
+        System.out.println(PaintJob.getBucketCount(3.4,2.1,1.5,2));
+        System.out.println(PaintJob.getBucketCount(2.75,3.25,2.5,1));
+    }
+
+}
